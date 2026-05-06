5ab9cefacd684a92cf7bef63dd22a7c18afa2c27
arifkoyani <arifkoyani@gmail.com>
2026-05-06 14:19:40 -0700

    import Navbar from "./components/navbar/navbar"; import Footer from "./components/footer/footer";

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+
 
 
 const geistSans = Inter({
